914881ef30571038f737e6a7e28563ddb917ab03
Matheus Passos <pinheiropassosm@gmail.com>
2021-04-05 12:05:07 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// Carregando Módulos
+const Twit = require('twit')
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+// Criando o bot
+const bot = new Twit({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+
+    timeout_ms: 60 * 100,
+})
+
+// Funções
+    // Configurações de pesquisa
+        function botIniciar () {
+        var query = {
+            q: 'pizza',
+            result_type: 'recent',
+            lang: 'pt',
+        }
+    
+    // Localiza tweets   
+        bot.get('search/tweets', query, localizaTT)
+
+        function localizaTT (error, data, response) {
+            if(error) {
+                console.log('Nao foi possivel achar os  tweets: ' + error)
+            } else {
+                var id = {
+                    id: data.statuses[0].id_str
+                }
+
+    // Retweet nos tweets encontrados 
+                bot.post('statuses/retweet/:id', id, retweetTT)
+                
+                function retweetTT(error, response) {
+                    if(error) {
+                        console.log('O bot não conseguiu dar Retweet: ' + console.error)
+                    } else {
+                        console.log('O bot deu retweet em: ' + id.id)
+                    }
+                }
+            }
+        }
+    }
+    
+    // Chama função pela primeira vez
+    botIniciar()
+    // Chama função depois de 5 minutos
+    setInterval(botIniciar, 5 * 60 * 1000)
+
+    // Servidor
+    const PORT = process.env.PORT || 8089
+    app.listen(PORT, () => {
+        console.log('Servidor rodando em: ' + PORT)
+    })
